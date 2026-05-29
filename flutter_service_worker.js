2f5dc88195b0ac7fa2144a43c3f21326eaa62b82
rightshiftsolutions <rightshiftsolutions@gmail.com>
2026-05-29 18:45:45 +0530

    Use generic templates for unmatched activity posters

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const BUILD_VERSION = '20260529130914';
+const BUILD_VERSION = '20260529131528';
 const APP_CACHE = 'gymgurus-flutter-app-' + BUILD_VERSION;
 const RUNTIME_CACHE = 'gymgurus-runtime-' + BUILD_VERSION;
 const CACHE_PREFIXES = [
