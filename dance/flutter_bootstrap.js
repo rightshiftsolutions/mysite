af850c9298cd49b1877f6dfd38c714cdd66b84f2
rightshiftsolutions <rightshiftsolutions@gmail.com>
2026-05-29 18:03:43 +0530

    Update DanceManager QR button build

--- dance/flutter_bootstrap.js
+++ dance/flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1584838555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2097135114" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
