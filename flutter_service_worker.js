9468930ce5f990ec1968068f5cedfe5d43441385
rightshiftsolutions <rightshiftsolutions@gmail.com>
2026-06-03 10:27:48 +0530

    Publish connectivity fix PWA

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const BUILD_VERSION = '20260602123611';
+const BUILD_VERSION = '20260603045711';
 const APP_CACHE = 'gymgurus-flutter-app-' + BUILD_VERSION;
 const RUNTIME_CACHE = 'gymgurus-runtime-' + BUILD_VERSION;
 const CACHE_PREFIXES = [
